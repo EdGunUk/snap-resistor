c12acb1760becb1c3b0bdaf2c342e747837b9b76
Eduard Hannoshyn <eduard.hannoshyn@avenga.com>
2022-06-18 21:55:36 +0300

    add animation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { logRoles } from "@testing-library/react";
 import { useCallback } from "react";
 import Snap from "snapsvg-cjs";
 
@@ -9,14 +8,14 @@ function App() {
 
             const style = ({
                 fill: '#387',
-                stroke: '#222',
+                stroke: '#fff',
                 strokeWidth: 5,
             });
 
             const path = paper
                 .path("")
                 .attr({
-                    stroke: '#222',
+                    stroke: '#387',
                     fill: 'transparent',
                     strokeWidth: 3
                 })
@@ -50,13 +49,33 @@ function App() {
                 updatePath(x, y, index);
             }
 
+            const onSircleStart = function (e) {
+                path.stop().animate({ opacity: .2 }, 200, window.mina.easeinout);
+                
+            }
+
+            const onSircleEnd = function (e) {
+                path.stop().animate({ opacity: 1 }, 200, window.mina.easeinout);
+            }
+
+            const onSircleMouseOver = function (e) {
+                this.stop().animate({ r: 20 }, 1000, window.mina.elastic);
+               
+            }
+
+            const onSircleMouseOut = function (e) {
+                this.stop().animate({ r: 15 }, 300, window.mina.easeinout);
+            }
+
             const onPaperClick = function (e) {
                 if (e.target.tagName !== 'circle') {
                     paper
                         .circle(e.clientX, e.clientY, 15)
                         .attr(style)
                         .data('i', coordsArray.length)
-                        .drag(onSircleDrag)
+                        .mouseover(onSircleMouseOver)
+                        .drag(onSircleDrag, onSircleStart, onSircleEnd)
+                        .mouseout(onSircleMouseOut)
 
 
                     updatePath(e.clientX, e.clientY);
