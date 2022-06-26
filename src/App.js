9bc43545d0a4b77faefe26388c890d08351a887d
eduard <edik95mt@gmail.com>
2022-06-27 00:05:16 +0300

    start work with radila nav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import {useCallback} from "react";
 import Snap from "snapsvg-cjs";
 
 function App() {
@@ -6,83 +6,27 @@ function App() {
         if (node !== null) {
             const paper = Snap(node);
 
-            const style = ({
-                fill: '#387',
-                stroke: '#fff',
-                strokeWidth: 5,
-            });
+            const style = {
+                fill: '#318199',
+                stroke: '#444',
+                strokeWidth: 4
+            }
 
-            const path = paper
-                .path("")
-                .attr({
-                    stroke: '#387',
-                    fill: 'transparent',
-                    strokeWidth: 3
+            const circle = paper
+                .circle(200, 200, 60)
+                .attr(style)
+
+            const rect = paper
+                .rect(300, 150, 100, 100)
+                .attr(style)
+
+            const group = paper
+                .g(circle, rect)
+                .attr({opacity: .5, strokeOpacity: .5})
+                .drag(function (dx) {
+                    this.transform(`r${dx}, 350 200`)
                 })
 
-            let coordsArray = [];
-
-            const updatePath = function (x, y, index) {
-                const coord = `${x},${y}`;
-
-                if (index !== undefined) {
-                    coordsArray[index] = coord;
-                } else {
-                    coordsArray.push(coord)
-                }
-
-
-                const [firstCoord, ...restCoords] = coordsArray;
-                let d = `M ${firstCoord}`;
-
-                for (const coord of restCoords) {
-                    d = `${d} L ${coord}`
-                }
-
-                path.attr({ d });
-            }
-
-            const onSircleDrag = function (dx, dy, x, y) {
-                this.attr({ cx: x, cy: y });
-                const index = this.data('i');
-
-                updatePath(x, y, index);
-            }
-
-            const onSircleStart = function (e) {
-                path.stop().animate({ opacity: .2 }, 200, window.mina.easeinout);
-                
-            }
-
-            const onSircleEnd = function (e) {
-                path.stop().animate({ opacity: 1 }, 200, window.mina.easeinout);
-            }
-
-            const onSircleMouseOver = function (e) {
-                this.stop().animate({ r: 20 }, 1000, window.mina.elastic);
-               
-            }
-
-            const onSircleMouseOut = function (e) {
-                this.stop().animate({ r: 15 }, 300, window.mina.easeinout);
-            }
-
-            const onPaperClick = function (e) {
-                if (e.target.tagName !== 'circle') {
-                    paper
-                        .circle(e.clientX, e.clientY, 15)
-                        .attr(style)
-                        .data('i', coordsArray.length)
-                        .mouseover(onSircleMouseOver)
-                        .drag(onSircleDrag, onSircleStart, onSircleEnd)
-                        .mouseout(onSircleMouseOut)
-
-
-                    updatePath(e.clientX, e.clientY);
-                }
-            }
-
-            paper.click(onPaperClick);
         }
     }, []);
 
