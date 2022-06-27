ca7127af4c95e2854b2087783c99e8b33155192b
Eduard Hannoshyn <eduard.hannoshyn@avenga.com>
2022-06-27 19:18:24 +0300

    transform variant

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import {useCallback} from "react";
 import Snap from "snapsvg-cjs";
 
 function App() {
@@ -6,83 +6,135 @@ function App() {
         if (node !== null) {
             const paper = Snap(node);
 
-            const style = ({
-                fill: '#387',
-                stroke: '#fff',
-                strokeWidth: 5,
-            });
-
+            let origTransform = 0;
             const path = paper
-                .path("")
+                .rect(100, 100, 100, 100)
                 .attr({
                     stroke: '#387',
                     fill: 'transparent',
                     strokeWidth: 3
                 })
+                .drag(function (dx, dy) {
+                    const m = Snap.matrix();
+                    m.translate(dx, dy);
+                    m.add(origTransform);
+                    m.rotate(dx, 150, 150);
+                    m.scale(.8, .8, 150, 150)
 
-            let coordsArray = [];
+                    this.transform(m);
+                }, function () {
+                    origTransform = this.transform().localMatrix;
+                })
 
-            const updatePath = function (x, y, index) {
-                const coord = `${x},${y}`;
+            let previousDx1 = 0;
+            let previousDy1 = 0;
+            let lastDx1 = 0;
+            let lastDy1 = 0;
+            let lastScale1 = .8;
 
-                if (index !== undefined) {
-                    coordsArray[index] = coord;
-                } else {
-                    coordsArray.push(coord)
-                }
+            const path1 = paper
+                .rect(100, 300, 100, 100)
+                .attr({
+                    stroke: 'red',
+                    fill: 'transparent',
+                    strokeWidth: 3
+                })
+                .drag(function (dx, dy) {
+                    const m = Snap.matrix();
+
+                    m.translate(dx + lastDx1, dy + lastDy1);
+                    m.rotate(dx + lastDx1, 150, 350);
+                    m.scale(lastScale1, lastScale1, 150, 350);
+
+                    previousDx1 = dx;
+                    previousDy1 = dy;
+                    this.transform(m);
+                }, function () {
+                }, function () {
+                    lastDx1 += previousDx1;
+                    lastDy1 += previousDy1;
+                    lastScale1 -= lastScale1 / 100 * 20;
+                })
+
+            let previousDx2 = 0;
+            let previousDy2 = 0;
+            let lastDx2 = 0;
+            let lastDy2 = 0;
+            let lastScale2 = .8;
+
+            const path2 = paper
+                .rect(100, 500, 100, 100)
+                .attr({
+                    stroke: 'blue',
+                    fill: 'transparent',
+                    strokeWidth: 3,
+                })
+                .drag(function (dx, dy) {
+                    this.transform(`t${dx + lastDx2} ${dy + lastDy2} r${dx + lastDx2} s${lastScale2} ${lastScale2}`);
+                    previousDx2 = dx;
+                    previousDy2 = dy;
+                }, function () {
+                }, function () {
+                    lastDx2 += previousDx2;
+                    lastDy2 += previousDy2;
+                    lastScale2 -= lastScale2 / 100 * 20;
+                })
+
+            let previousDx3 = 0;
+            let previousDy3 = 0;
+            let lastDx3 = 0;
+            let lastDy3 = 0;
+            let lastScale3 = .8;
+
+            const path3 = paper
+                .rect(100, 700, 100, 100)
+                .attr({
+                    stroke: 'orange',
+                    fill: 'transparent',
+                    strokeWidth: 3,
+                    'transform-origin': '150 750'
+                })
+                .drag(function (dx, dy) {
+                    this.transform(`translate(${dx + lastDx3},${dy + lastDy3})  rotate(${dx+ lastDx3}) scale(${lastScale3})`);
+
+                    previousDx3 = dx;
+                    previousDy3 = dy;
+                }, function () {
+                }, function () {
+                    lastDx3 += previousDx3;
+                    lastDy3 += previousDy3;
+                    lastScale3 -= lastScale3 / 100 * 20;
+                })
 
 
-                const [firstCoord, ...restCoords] = coordsArray;
-                let d = `M ${firstCoord}`;
+            let previousDx4 = 0;
+            let previousDy4 = 0;
+            let lastDx4 = 0;
+            let lastDy4 = 0;
+            let lastScale4 = .8;
 
-                for (const coord of restCoords) {
-                    d = `${d} L ${coord}`
-                }
+            const path4 = paper
+                .rect(100, 900, 100, 100)
+                .attr({
+                    stroke: 'pink',
+                    fill: 'transparent',
+                    'stroke-width': 3,
+                    'transform-origin': '150 950'
+                })
+                .drag(function (dx, dy) {
 
-                path.attr({ d });
-            }
+                    this.attr({
+                        transform: `translate(${dx + lastDx4},${dy + lastDy4})  rotate(${dx+ lastDx4}) scale(${lastScale4})`
+                    })
+                    previousDx4 = dx;
+                    previousDy4 = dy;
+                }, function () {
+                }, function () {
+                    lastDx4 += previousDx4;
+                    lastDy4 += previousDy4;
+                    lastScale4 -= lastScale4 / 100 * 20;
+                })
 
-            const onSircleDrag = function (dx, dy, x, y) {
-                this.attr({ cx: x, cy: y });
-                const index = this.data('i');
-
-                updatePath(x, y, index);
-            }
-
-            const onSircleStart = function (e) {
-                path.stop().animate({ opacity: .2 }, 200, window.mina.easeinout);
-                
-            }
-
-            const onSircleEnd = function (e) {
-                path.stop().animate({ opacity: 1 }, 200, window.mina.easeinout);
-            }
-
-            const onSircleMouseOver = function (e) {
-                this.stop().animate({ r: 20 }, 1000, window.mina.elastic);
-               
-            }
-
-            const onSircleMouseOut = function (e) {
-                this.stop().animate({ r: 15 }, 300, window.mina.easeinout);
-            }
-
-            const onPaperClick = function (e) {
-                if (e.target.tagName !== 'circle') {
-                    paper
-                        .circle(e.clientX, e.clientY, 15)
-                        .attr(style)
-                        .data('i', coordsArray.length)
-                        .mouseover(onSircleMouseOver)
-                        .drag(onSircleDrag, onSircleStart, onSircleEnd)
-                        .mouseout(onSircleMouseOut)
-
-
-                    updatePath(e.clientX, e.clientY);
-                }
-            }
-
-            paper.click(onPaperClick);
         }
     }, []);
 
