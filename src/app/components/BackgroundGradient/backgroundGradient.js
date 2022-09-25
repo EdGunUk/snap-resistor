582edeb5be52885d1c26266d25e378e86590c460
eduard.hannoshyn <edik95mt@gmail.com>
2022-09-25 15:25:20 +0300

    add drag functional

--- src/app/components/BackgroundGradient/backgroundGradient.js
+++ src/app/components/BackgroundGradient/backgroundGradient.js
@@ -6,7 +6,7 @@ const BackgroundGradient = () => {
         <>
             <radialGradient id="gradient" r='100%'>
                 <StopStyled stopColor={colors.WHITE} offset="0%"/>
-                <StopStyled stopColor={colors.SILVER} offset="100%"/>
+                <StopStyled stopColor={colors.GRAY} offset="100%"/>
             </radialGradient>
             <RectStyled as="rect" x="0" y="0"/>
         </>
