1d080a2213f4e3435b922e827a90aa4c8bd6c1a3
eduard.hannoshyn <edik95mt@gmail.com>
2022-10-02 13:54:45 +0300

    add meta theme-color

--- src/app/components/BackgroundGradient/backgroundGradient.js
+++ src/app/components/BackgroundGradient/backgroundGradient.js
@@ -1,12 +1,20 @@
 import {Container, Rect, Stop} from "./styled";
 import * as colors from "../../styled/settings/colors"
+import {useEffect} from "react";
 
 const BackgroundGradient = () => {
+
+    useEffect(() => {
+        const meta = document.querySelector('meta[name=theme-color]');
+        meta.setAttribute('content', colors.LIGHT_SILVER)
+    }, []);
+
+
     return (
         <Container as="svg">
             <radialGradient id="gradient" r='100%'>
                 <Stop stopColor={colors.WHITE} offset="0%"/>
-                <Stop stopColor={colors.GRAY} offset="100%"/>
+                <Stop stopColor={colors.LIGHT_SILVER} offset="50%"/>
             </radialGradient>
             <Rect as="rect" x="0" y="0"/>
         </Container>
