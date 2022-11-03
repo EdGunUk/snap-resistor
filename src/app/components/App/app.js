d71f4a4f45cc79bd95fa962ba2fddf1b5e6be5c5
eduard.hannoshyn <edik95mt@gmail.com>
2022-11-03 19:18:42 +0200

    update updateConfigInRange func

--- src/app/components/App/app.js
+++ src/app/components/App/app.js
@@ -22,28 +22,53 @@ const App = () => {
         translateY: 0,
         startClientY: 0,
         bandsEndTranslateY: [],
+        bandsReverse: []
     });
 
     useEffect(() => {
         setConfig((config) => updateConfig({baseConfig, config}));
     }, [baseConfig])
 
-    const updateConfigInRange = (props) => {
-        const {baseConfig, bandId} = props;
-        const updatedConfig = updateConfig(props);
-        const updatedBand = updatedConfig[bandId];
-        const baseBand = baseConfig[bandId];
+    const updateConfigInRange = () => {
+        const {current} = dragData;
+        const {bandId, translateY, bandsEndTranslateY, bandsReverse} = current;
+        const configClone = [...config];
+        const band = config[bandId]
+        const baseBand = baseConfig[bandId]
         const reversedBand = reversedConfig[bandId];
+        const isReverse = bandsReverse[bandId] ?? false;
 
-        if (updatedBand[updatedBand.length - 1].pathData.y < reversedBand[0].pathData.y) {
-            updatedConfig[bandId] = reversedBand;
+
+        if (isReverse) {
+            if (band[band.length - 1].pathData.y > baseBand[0].pathData.y) {
+                configClone[bandId] = baseBand;
+                setConfig(configClone);
+                bandsEndTranslateY[bandId] = 0
+                bandsReverse[bandId] = false;
+            } else if(band[0].pathData.y < reversedBand[0].pathData.y){
+                configClone[bandId] = reversedBand;
+                setConfig(configClone);
+                bandsEndTranslateY[bandId] = 0
+                bandsReverse[bandId] = true;
+            }
+            else {
+                bandsEndTranslateY[bandId] = translateY;
+            }
+        } else {
+            if (band[band.length - 1].pathData.y < reversedBand[0].pathData.y) {
+                configClone[bandId] = reversedBand;
+                setConfig(configClone);
+                bandsEndTranslateY[bandId] = 0
+                bandsReverse[bandId] = true;
+            } else if (band[0].pathData.y > baseBand[0].pathData.y) {
+                configClone[bandId] = baseBand;
+                setConfig(configClone);
+                bandsEndTranslateY[bandId] = 0
+                bandsReverse[bandId] = false;
+            } else {
+                bandsEndTranslateY[bandId] = translateY;
+            }
         }
-
-        if (updatedBand[0].pathData.y > baseBand[0].pathData.y) {
-            updatedConfig[bandId] = baseBand;
-        }
-
-        return updatedConfig;
     }
 
 
@@ -59,26 +84,32 @@ const App = () => {
 
     const handlePointerMove = (e) => {
         const {current} = dragData;
-        const {bandId, startClientY, bandsEndTranslateY} = current;
+        const {bandId, startClientY, bandsEndTranslateY, bandsReverse} = current;
         if (!bandId) return;
 
+        const isReverse = bandsReverse[bandId] ?? false;
         const endTranslateY = bandsEndTranslateY[bandId] ?? 0;
         const translateY = e.clientY - startClientY + endTranslateY;
 
-        setConfig((config) => updateConfigInRange({baseConfig, config, bandId, translateY}));
+        setConfig((config) => updateConfig({
+            config,
+            baseConfig: isReverse ? reversedConfig : baseConfig,
+            bandId,
+            translateY,
+            isReverse
+        }));
         current.translateY = translateY;
     }
 
     const handlePointerUp = () => {
         const {current} = dragData;
-        const {bandId, bandsEndTranslateY} = current;
+        const {bandId} = current;
         if (!bandId) return;
 
+        updateConfigInRange();
         setCursor(cursorTypes.AUTO);
-        bandsEndTranslateY[bandId] = current.translateY;
         current.bandId = null;
     }
-
 
     return (
         <Main
